f0b15bf8c4a41b27ec113be0039d2522d95d62a8
Vidhi Vaks <vidhi.vakharia93@gmail.com>
2019-09-25 08:06:34 -0400

    created app.js and index.js in client

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 import Jumbotron from "./components/Jumbotron";
 import Nav from "./components/Nav";
-
+import Footer from "./components/Footer";
 
 class App extends Component {
-
-
-
-
-
-    
   render() {
     return (
       <div>
         <Nav />
         <Jumbotron />
+        <Footer />
       </div>
     );
   }
