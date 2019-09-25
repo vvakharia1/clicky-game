2c8c3fba1f8459d14d5c128f9f4ae39b159d121d
Vidhi Vaks <vidhi.vakharia93@gmail.com>
2019-09-25 10:10:17 -0400

    set state of images to be displayed

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,28 @@ import Jumbotron from "./components/Jumbotron";
 import Nav from "./components/NavBar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
-
+import images from "./planets.json";
+import PlanetCard from "./components/Card";
 
 class App extends Component {
+  state = {
+    images
+  };
+
   render() {
     return (
       <div>
         <Nav />
         <Jumbotron />
-        <Wrapper 
-        />
+        <Wrapper>
+          {this.state.images.map(images => (
+            <PlanetCard
+              key={images.id}
+              image={images.image}
+              name={images.name}
+            />
+          ))}
+        </Wrapper>
         <Footer />
       </div>
     );
