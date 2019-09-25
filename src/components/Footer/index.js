23a41c634a88ac7d65f452e5326fb4427eb2c4ec
Vidhi Vaks <vidhi.vakharia93@gmail.com>
2019-09-25 07:46:45 -0400

    created Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer(props) {
+  return (
+    <footer className="text-center mt-auto">
+      <div className="h-font footerText">© 2019 Vidhi Vakharia</div>
+    </footer>
+  );
+}
+
+export default Footer;
